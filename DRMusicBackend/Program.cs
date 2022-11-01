5d2e08c6d8e24a9ac3eabba3177db0a743211a19
SophusS <soph9579@edu.zealand.dk>
2022-11-01 13:47:16 +0100

    Update Program.cs

--- DRMusicBackend/Program.cs
+++ DRMusicBackend/Program.cs
@@ -1,4 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
+var policyName = "_myAllowSpecificOrigins";
 //Ion ssdf
 // sophus was here twice
 // Add services to the container.
@@ -7,7 +8,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: policyName,
+                      builder =>
+                      {
+                          builder
+                            .WithOrigins("http://localhost:3000") // specifying the allowed origin
+                            .WithMethods("GET") // defining the allowed HTTP method
+                            .AllowAnyHeader(); // allowing any header to be sent
+                      });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
